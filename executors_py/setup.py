48c44a86e1c7f2573ef9c3ff501816b4539ab58f
Diogo <diogo@DiegoS.localdomain>
2026-02-23 20:33:55 -0500

    ***** EXECUTORS *** se añaden los archivos de clase

---
+++ executors_py/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'executors_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='diogo',
+    maintainer_email='diogo@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "single_threaded_executor = executors_py.single_threaded_executor:main",
+            "multi_threaded_executor = executors_py.multi_threaded_executor:main"
+        ],
+    },
+)
