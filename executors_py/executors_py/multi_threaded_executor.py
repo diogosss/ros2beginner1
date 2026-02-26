1361f6d1c23cc7a2d762bf733c2f5de864362390
Diogo <diogo@DiegoS.localdomain>
2026-02-25 19:33:13 -0500

    Ejercicio con tres grupos de callbacks y 2 nodos en el mismo executable

--- executors_py/executors_py/multi_threaded_executor.py
+++ executors_py/executors_py/multi_threaded_executor.py
@@ -30,12 +30,12 @@ class Node1(Node):
         #3 ¿Timer 1 paralelo a Timer 2?->SI	
         # ¿Timer 1 paralelo a sí mismo?->No (Seguro)	
         # ¿Uso de CPU/Hilos?->Equilibrado
-        self.cb_group2_ = MutuallyExclusiveCallbackGroup()
         self.cb_group1_ = MutuallyExclusiveCallbackGroup()
-        self.cb_group3_ = MutuallyExclusiveCallbackGroup()
+        self.cb_group2_ = MutuallyExclusiveCallbackGroup()        
+        #self.cb_group3_ = MutuallyExclusiveCallbackGroup()
         self.timer1_ = self.create_timer(1.0, self.callback_timer1, callback_group=self.cb_group1_)
         self.timer2_ = self.create_timer(1.0, self.callback_timer2, callback_group=self.cb_group2_)
-        self.timer3_ = self.create_timer(1.0, self.callback_timer3, callback_group=self.cb_group3_)
+        self.timer3_ = self.create_timer(1.0, self.callback_timer3, callback_group=self.cb_group2_)
 
     def callback_timer1(self):
         time.sleep(2.0)
@@ -49,11 +49,28 @@ class Node1(Node):
         time.sleep(2.0)
         self.get_logger().info("cb 3")
 
+# NODO 2
+class Node2(Node):
+    def __init__(self):
+        super().__init__("node2")
+        self.cb_group4_ = ReentrantCallbackGroup()
+        self.timer4_ = self.create_timer(1.0, self.callback_timer4, callback_group=self.cb_group4_ )
+        self.timer5_ = self.create_timer(1.0, self.callback_timer5, callback_group=self.cb_group4_ )
+
+    def callback_timer4(self):
+        time.sleep(2.0)
+        self.get_logger().info("cb 4")
+
+    def callback_timer5(self):
+        time.sleep(2.0)
+        self.get_logger().info("cb 5")
+
 
 def main(args=None):
     rclpy.init(args=args)
     
     node1 = Node1()
+    node2 = Node2()
     #1
     #rclpy.spin(node1)
     
@@ -62,11 +79,20 @@ def main(args=None):
     #3 
     executor = MultiThreadedExecutor()
     executor.add_node(node1)
-    executor.spin()
-
-    
+    executor.add_node(node2)
+    executor.spin()   
     rclpy.shutdown()
 
 
 if __name__ == "__main__":
     main()
+
+# 1
+# En este caso, no hay restricciones. Un grupo reentrante permite que sus callbacks se ejecuten en paralelo entre sí,
+# e incluso que múltiples instancias del mismo callback se ejecuten simultáneamente si el anterior no ha terminado.
+
+#2
+# Aquí divides las reglas de juego. timer1 es libre, pero timer2 y timer3 comparten una restricción.
+
+#3
+# Este es el diseño más común cuando quieres paralelismo pero quieres proteger cada función de sí misma (que una función no se pise a sí misma).
