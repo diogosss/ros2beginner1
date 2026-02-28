cb9382034ef7dab3843acebd8e780a916c0b79b8
Diogo <diogo@DiegoS.localdomain>
2026-02-28 18:24:30 -0500

    Solucion Profesor, se añade parametro para nombre

--- final_project_cpp/src/turtle_controller.cpp
+++ final_project_cpp/src/turtle_controller.cpp
@@ -5,50 +5,69 @@
 using namespace std::chrono_literals;
 using namespace std::placeholders;
 
-class TurtleControllerNode : public rclcpp::Node  
+class TurtleController : public rclcpp::Node  
 {
 public:
-    TurtleControllerNode() : Node("turtle_controller_client")   
+    TurtleController() : Node("turtle_controller")   
     {
-        kill_client_ = this->create_client<turtlesim::srv::Kill>("kill");
-        spawn_client_ = this->create_client<turtlesim::srv::Spawn>("spawn");
+        this->declare_parameter("turtle_name", rclcpp::PARAMETER_STRING);
+        turtle_name_ = this->get_parameter("turtle_name").as_string();
+
+        cb_group_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+
+        kill_turtle_client_ = this->create_client<turtlesim::srv::Kill>("/kill", rclcpp::ServicesQoS(), cb_group_);
+        spawn_turtle_client_ = this->create_client<turtlesim::srv::Spawn>("/spawn",rclcpp::ServicesQoS(), cb_group_);
+
+        //Test desde constructor con threads
+        spawn_turtle_thread_ = std::thread(std::bind(&TurtleController::spawnTurtle, this, 5.5, 5.5, 0.0));
+        
+
     }
 
-    void killTurtle(std::string name)
+    
+
+private:
+
+    void killTurtle()
     {
-        while (!kill_client_->wait_for_service(1s))
+        while (!kill_turtle_client_->wait_for_service(1s))
         {
             RCLCPP_WARN(this->get_logger(), "Waiting for the server...");
         }
 
         auto request = std::make_shared<turtlesim::srv::Kill::Request>();
-        request->name = name;
+        request->name = turtle_name_;
 
-        kill_client_ -> async_send_request(request, std::bind(&TurtleControllerNode::callbackKillTurtle, this, _1));
+        RCLCPP_WARN(this->get_logger(), "Trying to remove turtle...");
+
+        kill_turtle_client_ -> async_send_request(request, std::bind(&TurtleController::callbackKillTurtle, this, _1));
     }
 
-    void spawnTurtle(double x, double y, double theta, std::string name)
+    void spawnTurtle(double x, double y, double theta)
     {
-        while (!spawn_client_->wait_for_service(1s))
+        while (!spawn_turtle_client_->wait_for_service(1s))
         {
             RCLCPP_WARN(this->get_logger(), "Waiting for the server...");
         }
 
         auto request = std::make_shared<turtlesim::srv::Spawn::Request>();
-        request->name = name;
+        request->name = turtle_name_;
         request->x = x;
         request->y = y;
         request->theta = theta;
+        RCLCPP_WARN(this->get_logger(), "Trying to spawn turtle...");
 
-        spawn_client_ -> async_send_request(request, std::bind(&TurtleControllerNode::callbackSpawnTurtle, this, _1));
+        spawn_turtle_client_ -> async_send_request(request, std::bind(&TurtleController::callbackSpawnTurtle, this, _1));
+
+        //ejecutar el metodo de Kill luego de spawn
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        kill_turtle_thread_ = std::thread(std::bind(&TurtleController::killTurtle, this));
     }
-
-private:
 
     void callbackKillTurtle(rclcpp::Client<turtlesim::srv::Kill>::SharedFuture future)
     {
         auto response = future.get();
-        //RCLCPP_INFO(this->get_logger(), "Kill Turtle Response: %d", (int)response->sum);
+        RCLCPP_INFO(this->get_logger(), " Turtle  has been removed");
     }
 
     void callbackSpawnTurtle(rclcpp::Client<turtlesim::srv::Spawn>::SharedFuture future)
@@ -57,17 +76,23 @@ private:
         RCLCPP_INFO(this->get_logger(), "Spawn Turtle Response: %s", response->name.c_str());
     }
 
-    rclcpp::Client<turtlesim::srv::Kill>::SharedPtr kill_client_;
-    rclcpp::Client<turtlesim::srv::Spawn>::SharedPtr spawn_client_;
+    rclcpp::Client<turtlesim::srv::Kill>::SharedPtr kill_turtle_client_;
+    rclcpp::Client<turtlesim::srv::Spawn>::SharedPtr spawn_turtle_client_;
+    std::string turtle_name_;
+
+    std::thread spawn_turtle_thread_;
+    std::thread kill_turtle_thread_;
+
+    rclcpp::CallbackGroup::SharedPtr cb_group_;
 
 };
 
 int main(int argsc, char **argv){
     rclcpp::init(argsc, argv);
-    auto node = std::make_shared<TurtleControllerNode>();  
-    node -> killTurtle("turtle1");
-    node -> spawnTurtle(5.0,5.0, 0.0, "turtle2");
-    rclcpp::spin(node);
+    auto node = std::make_shared<TurtleController>();  
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(node);
+    executor.spin();
     rclcpp::shutdown();
     return 0;
 }
